4bd6dc90887492c5e31813c87938447abfc6db48
Rossmel Abasto <abastorossmel@gmail.com>
2023-09-18 03:08:38 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+// Variables globales
+const processQueue = [];
+const queueElement = document.getElementById('queue');
+
+// Función para agregar un proceso a la cola
+function addProcess() {
+    const processId = generateUniqueId();
+    processQueue.push(processId);
+
+    const processElement = document.createElement('div');
+    processElement.classList.add('process');
+    processElement.setAttribute('data-id', processId);
+    const randomIcon = getRandomIcon();
+    processElement.innerHTML = `<i class="${randomIcon}"></i><span>${processId}</span>`;
+
+    queueElement.appendChild(processElement);
+}
+
+// Función para ejecutar un proceso de la cola
+function executeProcess() {
+    if (processQueue.length === 0) {
+        return;
+    }
+
+    const executedProcessId = processQueue.shift();
+    const executedProcessElement = document.querySelector(`.process[data-id="${executedProcessId}"]`);
+
+    if (executedProcessElement) {
+        executedProcessElement.remove();
+    }
+}
+
+// Variable global para mantener un contador
+let idCounter = 1;
+
+// Función para generar un ID único en orden con 3 cifras
+function generateUniqueId() {
+    const paddedId = String(idCounter).padStart(3, '0');
+    idCounter++;
+    return paddedId;
+}
+
+// Función para obtener un icono aleatorio de Font Awesome
+function getRandomIcon() {
+    const icons = [
+        'fas fa-desktop',
+        'fas fa-mouse',
+        'fas fa-keyboard',
+        'fas fa-hdd',
+        'fas fa-microchip',
+        'fas fa-print',
+        'fas fa-headphones',
+        'fas fa-camera',
+        'fas fa-wifi',
+        'fas fa-sim-card',
+        'fas fa-memory',
+        'fas fa-database',
+        'fas fa-server',
+        'fas fa-cloud',
+
+    ];
+    const randomIndex = Math.floor(Math.random() * icons.length);
+    return icons[randomIndex];
+}
+
+// Event Listeners
+document.getElementById('addProcess').addEventListener('click', addProcess);
+document.getElementById('executeProcess').addEventListener('click', executeProcess);
